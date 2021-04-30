4744da627f77f28557334542e24c54bdfbdbc90e
Alan Vekselman <avekselman@itba.edu.ar>
2021-04-30 10:59:30 -0300

    Added Progress-Bar and fixed bug

--- TP2/Synthesis/Program/GUIs/GUI.py
+++ TP2/Synthesis/Program/GUIs/GUI.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QProgressBar
 from GUIs.Player_GUI.Player import Player
 from GUIs.GUI_QT import Ui_Form
 from File.File import open_file
@@ -7,7 +7,7 @@ from GUIs.TrackInfo.TrackInfo import TrackInfo
 from Instruments.Karplus_Strong.KSInstrument import Guitar, Drum, Harp
 import numpy as np
 from threading import Thread
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtCore import Qt
 
 
 class GUI(QWidget, Ui_Form):
@@ -28,10 +28,16 @@ class GUI(QWidget, Ui_Form):
         self.newlay = QVBoxLayout()
         self.player = Player(self.GUI_Frame)
         self.newlay.addWidget(self.player, 0, Qt.AlignHCenter)
+
+        self.progress = QProgressBar(self.GUI_Frame)
+        self.newlay.addWidget(self.progress)
+        self.progress.setValue(0)
+
         self.Buttons_Layout.addLayout(self.newlay)
         self.th = Thread(target = self._synth)
 
         self.scroller.setFixedWidth(250)
+        self.sound = np.array([])
 
     def get_file(self):
         self.Synth_Button.setEnabled(False)
@@ -39,6 +45,7 @@ class GUI(QWidget, Ui_Form):
         if not len(self.filename): return
 
         self.Synth_Button.setEnabled(True)
+        self.progress.setValue(0)
         for i, track in enumerate(self.track_tracker):
             self.track_layout.removeWidget(track)
             track.delete()
@@ -53,20 +60,28 @@ class GUI(QWidget, Ui_Form):
     def synthesize(self):
         if len(self.filename):
             if self.th.is_alive(): self.th.join()
+
+            self.player.stop()
+            self.player.Play_Button.setEnabled(False)
+            self.Synth_Button.setEnabled(False)
+            self.File_Button.setEnabled(False)
+
             self.th = Thread(target = self._synth)
             self.th.start()
 
     def _synth(self):
-        if len(self.filename):
-            self.player.stop()
-            self.player.Play_Button.setEnabled(False)
 
-            used = []
+        used, tot = [], 0
+        for track in self.track_tracker:
+            if track.isChecked(): tot += 1
+        curr = 0
+        self.progress.setValue(0)
 
+        if tot:
             for i, track in enumerate(self.track_tracker):
                 if track.isChecked():
                     index = self.instrument_names.index(track.current_option())
-                    print(f'Synthesizing track {i+1} of {len(self.track_tracker)} with: {self.instrument_names[index]}')
+                    print(f'Synthesizing track {curr + 1} of {tot} with: {self.instrument_names[index]}')
 
                     if index in used:
                         self.instruments[index].synthesize(fs = 48000, track = i, add = True)
@@ -74,17 +89,20 @@ class GUI(QWidget, Ui_Form):
                         self.instruments[index].load(self.file)
                         self.instruments[index].synthesize(fs = 48000, track = i)
                         used.append(index)
+                    curr += 1
+                    self.progress.setValue(int(curr / tot * 100))
 
             for pos, i in enumerate(used):
-                if not pos: self.sound = self.instruments[i].sound.copy()
-                else:
-                    if self.sound.size < self.instruments[i].sound.size:
-                        self.sound = np.append(self.sound, np.zeros(self.instruments[i].sound.size - self.sound.size))
-
-                    self.sound[ : self.instruments[i].sound.size] += self.instruments[i].sound
+                if self.sound.size < self.instruments[i].sound.size:
+                    self.sound = np.append(self.sound, np.zeros(self.instruments[i].sound.size - self.sound.size))
+                if pos: self.sound[ : self.instruments[i].sound.size] += self.instruments[i].sound
+                else: self.sound[ : self.instruments[i].sound.size] = self.instruments[i].sound
 
             self.player.load(self.sound, 48000)
             print('Synthesized')
+
+            self.Synth_Button.setEnabled(True)
+            self.File_Button.setEnabled(True)
 
     def keyPressEvent(self, a0) -> None:
         if a0.key() == Qt.Key_Escape: self.close()
