a1c0185a682d57866f67bdebad3700e107364351
Alan Vekselman <avekselman@itba.edu.ar>
2021-05-12 00:24:14 -0300

    Added effects

--- TP2/Synthesis/Program/GUIs/Player_GUI/Player.py
+++ TP2/Synthesis/Program/GUIs/Player_GUI/Player.py
@@ -3,8 +3,13 @@ from threading import Thread
 import numpy as np
 from PyQt5.QtWidgets import QWidget, QStyle
 from GUIs.Player_GUI.Player_QT import Ui_Form
+from GUIs.Player_GUI.Effects import efecto
 
-# Clase reproductor. 
+# echo: Recibe fs, tau < 1, t60, zi
+# freeverb: Recibe N, f < 1, d < 1, zi
+# flanger: full_data, i * chunksize, delay : int, range_sound : int, sweep_freq : float
+
+# Clase reproductor.
 class Player(QWidget, Ui_Form):
     
     CHUNKSIZE = 1024
@@ -40,9 +45,19 @@ class Player(QWidget, Ui_Form):
         self.Stop_Button.setEnabled(False)
         self.Stop_Button.repaint()
 
+        self.zi = None
+        self.effect_generator = efecto()
+
+
+        self.flanger_check.clicked.connect(self.reset_zi)
+        self.freeverb_check.clicked.connect(self.reset_zi)
+        self.echo_check.clicked.connect(self.reset_zi)
+
+    def reset_zi(self, keep):
+        if not keep: self.zi = None
+
     # Carga de nuevo sonido
     def load(self, data, fs):
-
 
         self.Play_Button.setEnabled(False)
         self.Pause_Button.setEnabled(False)
@@ -66,19 +81,39 @@ class Player(QWidget, Ui_Form):
 
     # Thread de loopeo. Va tomando datos de a CHUNKSIZE y mandándolos al stream
     def _keep_playing(self):
-        data = self.sound[ : self.CHUNKSIZE].tobytes()
+        data = self.sound[ : self.CHUNKSIZE]
+
+        self.zi = None
+        parameters = self.gen_parameters(data, 0)
+
+        if parameters is not None: data_play, self.zi = self.effect_generator.effect_select(parameters)
+        else: data_play = data
+
+        data_play = data_play.tobytes()
         self.idx += 1
         curr_min = 0
         
         while len(data) and not self.stop_playing:
             if not self.paused:
-                self.stream.write(data)
+
+                self.stream.write(data_play)
                 data = self.sound[self.idx * self.CHUNKSIZE : (self.idx + 1) * self.CHUNKSIZE]
                 curr_min += data.size / self.fs / 60
-                data = data.tobytes()
+
+                parameters = self.gen_parameters(data, self.idx)
+                if parameters is not None:
+                    data_play, self.zi = self.effect_generator.effect_select(parameters)
+                    data_play = data_play.astype(np.float32)
+
+                    if np.any(data_play > 1):
+                        data_play = (data_play / np.abs(data_play).max())
+
+                else:
+                    data_play = data
+
+                data_play = data_play.tobytes()
 
                 self.idx += 1
-
                 minutes = int(curr_min)
                 seconds = int((curr_min - minutes) * 60)
 
@@ -136,6 +171,48 @@ class Player(QWidget, Ui_Form):
             self.Pause_Button.setEnabled(False)
 
     def playing(self): return self.th.is_alive()
+
+    def gen_parameters(self, data, i):
+        if self.echo_check.isChecked():
+            return \
+            {
+                'echo':
+                    (
+                        self.fs,
+                        data,
+                        self.tau_slider.value() * 1e-3,
+                        self.t60_slider.value(),
+                        self.zi
+                    )
+            }
+        if self.freeverb_check.isChecked():
+            return \
+                {
+                'freeverb' :
+                    (
+                        data,
+                        self.N_slider.value(),
+                        self.f_slider.value() * 1e-2,
+                        self.d_slider.value() * 1e-2,
+                        self.zi
+                    )
+                }
+        if self.flanger_check.isChecked():
+            return \
+                {
+
+                'flanger' :
+                    (
+                        self.sound,
+                        data,
+                        i * self.CHUNKSIZE,
+                        self.delay_slider.value(),
+                        self.range_slider.value(),
+                        self.sweep_slider.value() / 8,
+                        self.fs
+                    )
+                }
+        return None
 
     # Libera recursos de PyAudio. Llamar siempre antes de cerrar el programa.
     def close(self): 
