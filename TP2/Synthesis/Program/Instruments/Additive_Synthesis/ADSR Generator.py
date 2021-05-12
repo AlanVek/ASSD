51a2a5db0eeb9d4a59147816b110c2a6d12ff198
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2021-05-12 19:44:03 -0300

    Sintesis Aditiva V2.0

--- TP2/Synthesis/Program/Instruments/Additive_Synthesis/ADSR Generator.py
+++ TP2/Synthesis/Program/Instruments/Additive_Synthesis/ADSR Generator.py
@@ -4,7 +4,8 @@ from Instruments.Additive_Synthesis.ADSR_Note_Generator import ADSR_Note_Generat
 class ADSR_instrument(Instrument):
 
     def _gen_note(self, freq, dur, **kwargs):
-        return ADSR_Note_Generator(self.Amp,self.A,self.D,self.S,self.R,self.freq,self.dur,self.fs);
+        norm = 440/freq #Corrección arbitraria pero que a grandes rasgos corrije bien cómo varían los ADSR de mayores y menores frecuencias a 440Hz.
+        return ADSR_Note_Generator(self.Amp*norm,self.A*norm,self.D*norm,self.S*norm,self.R*norm,self.freq,self.dur,self.fs);
 
 class Píano(ADSR_instrument):
 
@@ -13,5 +14,5 @@ class Píano(ADSR_instrument):
         self.D = 0.2;
         self.S = 0.3;
         self.R = 0.2;
-        self.Amp = [1, 0.561, 0.355, 0.0631, 0.02, 0.00631, 0.00369]
+        self.Amp = [1, 0.561, 0.355, 0.0631, 0.02, 0.00631, 0.00369] #Estoy pensando si no pasar una matriz de dB para los harmónicos de cada nota, estos son de La.
          #Obs: Si suelto la tecla antes de entrar a sustain, se saltea sustain.
--- TP2/Synthesis/Program/Instruments/Additive_Synthesis/ADSR_Note_Generator.py
+++ TP2/Synthesis/Program/Instruments/Additive_Synthesis/ADSR_Note_Generator.py
@@ -5,66 +5,95 @@ from IPython.display import Audio
 from random import seed
 from random import randint
 
+
 def ADSR_Note_Generator(Amp: np.array, A: float,D: float,S: float,R: float,freq: float,dur: float,fs: float) -> np.array:
 
-    max_sustain = 2 #Duración máxima del sustain, seleccionado artesanalmente.
-    #Es un magic number, la verdad, pero esta sección salió a base de prueba y error empíricamente.
+    seed()
+    # norm = 440/freq; #Esto tengo que ver cómo hago luego
+    norm = 1;
+
+    S = S * norm  # El sustain es un porcentaje de la amplitud máxima a la que llega en el ataque. Defino arbitrariamente este valor ya que suena bien.0.32 0.1
+
+    # maxSustainFlag = False;
+    max_sustain = 2 * norm  # Duración máxima del sustain
 
     releaseTime = 0
     envelope = 0
 
+    attackRate = 1 / A  # La amplitud máxima sobre el tiempo que el toma llegar.
+    decayRate = (1 - S) / D  # Misma idea
+    releaseRate = S / R  # Asumo que el tiempo de release normal va a ser cuando el sustain está en su valor máximo S y quiero que baje en R segundos.
 
-    attackRate = 1/A #La amplitud máxima sobre el tiempo que el toma llegar.
-    decayRate = (1-S)/D #Misma idea
-    releaseRate = S/R #Asumo que el tiempo de release normal va a ser cuando el sustain está en su valor máximo S y quiero que baje en R segundos.
-
-    if(dur < A + D ): #Si no llego al sustain, entro a este caso.
-        if(dur > A): #Si completo el ataque, recorto el decay
+    if (dur < A + D):  # Si no llego al sustain, entro a este caso.
+        if (dur > A):  # Si completo el ataque, recorto el decay
             D = dur - A;
             S = 0;
-            minDecay = 1 - decayRate * D; #Me fijo hasta donde baja después del ataque hasta el momento que se corta.
-            releaseTime = minDecay/releaseRate #El tiempo de release va a ser el tiempo que tarda en bajar con la pendiente calculada desde minDecay hasta cero.
-            attack = np.linspace(0, 1, int(A * fs)); #Creo el envelope.
+            minDecay = 1 - decayRate * D;  # Me fijo hasta donde baja después del ataque hasta el momento que se corta.
+            # releaseTime = minDecay/releaseRate #El tiempo de release va a ser el tiempo que tarda en bajar con la pendiente calculada desde minDecay hasta cero.
+            attack = np.linspace(0, 1, int(A * fs));  # Creo el envelope.
             decay = np.linspace(1, minDecay, int(D * fs))
-            release = np.linspace(minDecay, 0, int(releaseTime *fs))
-            envelope = np.concatenate((attack, decay,release))#Concateno las rectas de la envolvente.
-        else: #Si no, elimino el decay y recorto el ataque directamente.
+            # release = np.linspace(minDecay, 0, int(releaseTime *fs))
+            envelope = np.concatenate((attack, decay))  # Concateno las rectas de la envolvente.
+        else:  # Si no, elimino el decay y recorto el ataque directamente.
             D = 0
-            S= 0
+            S = 0
+            print("hey arnold")
             A = dur
-            maxAttack = attackRate * dur
-            releaseTime = (maxAttack)/releaseRate
-            attack = np.linspace(0, maxAttack, int(A * fs))
-            release = np.linspace(maxAttack, 0, int(releaseTime *fs))
-            envelope = np.concatenate((attack, release))#Concateno las rectas de la envolvente.
+            maxAttack = attackRate * dur;
+            # releaseTime = (maxAttack)/releaseRate
+            attack = np.linspace(0, maxAttack, int(A * fs));
+            # release = np.linspace(maxAttack, 0, int(releaseTime *fs))
+            envelope = attack  # Concateno las rectas de la envolvente.
     else:
-        sustain_time = dur - A - D; #El sustain time asumiendo que fuese ideal y no decae. Luego elegiré el mínimo entre los dos.
-        alpha = S/max_sustain #Fall rate del sustain. Puede ser que llegue a 0 antes del release, cuyo caso se contempla.
-        t = np.linspace(0,max_sustain,int(fs*(max_sustain))) # Tiempo total
-        Sf = S - t*alpha; #Asumo que desciende linealmente el sustain. Con el dur*alpha el
-        index = Sf < S/100
+        sustain_time = dur - A - D  # El sustain time asumiendo que fuese ideal y no decae. Luego elegiré el mínimo entre los dos.
+        alpha = S / max_sustain  # Fall rate del sustain. Puede ser que llegue a 0 antes del release, cuyo caso se contempla.
+        t = np.linspace(0, max_sustain, int(fs * (max_sustain)));  # Tiempo total
+        Sf = S - t * alpha  # Asumo que desciende linealmente el sustain. Con el dur*alpha el
+        index = Sf < S / 100
         minIndex = np.argmax(index)
         Sf[index] = 0
-        attack = np.linspace(0, 1, int(A * fs))
+        attack = np.linspace(0, 1, int(A * fs));
         decay = np.linspace(1, S, int(D * fs))
-        if not minIndex:
-            sustain = Sf[ : int(sustain_time*fs)]
-            releaseTime = Sf[int(sustain_time*fs)]/releaseRate;
-            release = np.linspace(Sf[int(sustain_time*fs)], 0, int(releaseTime * fs));
-            envelope = np.concatenate((attack, decay,sustain,release))#Concateno las rectas de la envolvente.
+
+        if not minIndex or int(sustain_time * fs) <= minIndex:
+            sustain = Sf[: int(sustain_time * fs)]
+            #releaseTime = Sf[int(sustain_time * fs)] / releaseRate;
+            # release = np.linspace(Sf[int(sustain_time*fs)], 0, int(releaseTime * fs));
+            envelope = np.concatenate((attack, decay, sustain))  # Concateno las rectas de la envolvente.
+
         else:
-            sustain = Sf[ : minIndex]
-            releaseTime = Sf[-1]/releaseRate
-            release = np.linspace(Sf[-1], 0, int(releaseTime * fs))
-            envelope = np.concatenate((attack, decay,sustain,release))#Concateno las rectas de la envolvente.
+            sustain = Sf[: minIndex];
+            #releaseTime = Sf[-1] / releaseRate;
+            print(Sf[minIndex])
+            # release = np.linspace(Sf[minIndex - 1], 0, int(releaseTime * fs));
+            envelope = np.concatenate((attack, decay, sustain))  # Concateno las rectas de la envolvente.
 
-    t = np.linspace(0,dur+releaseTime,int(fs*(dur+releaseTime))) # Tiempo total
-    s = np.zeros(int(fs*(dur+releaseTime))) #Creo arreglo para la señal que será mi nota de piano.
+    release = np.zeros(int(dur * fs))
 
-    for i in range(len(Amp)):
-        s += Amp[i]*np.sin(2*np.pi*((freq)*i*t));
+    for i in range(len(release)):
+        release[-(i + 1)] = (i * releaseRate) / fs;
+
+    t = np.linspace(0, dur, int(fs * dur));  # Tiempo total
+    s = np.zeros(int(fs * dur));  # Creo arreglo para la señal que será mi nota de piano.
+
+    # x = [-48,-36,-36,-30,-32,-30,-32,-35,-37,-46,-45,-51,-55,-55,-58,-57,-65,-66,-66]
+    # Amp = np.zeros(np.size(x));
+
+    # for i in range(len(x)):
+    #    Amp[i] = 10**(x[i]/20)
+
+    # print(Amp)
+
+    for i in range(1, len(Amp) + 1):
+        s += Amp[i - 1] * np.sin(2 * np.pi * (freq) * i * t);
+        # + randint(0,1)
+
+    if (s.size > envelope.size):
+        envelope = np.append(envelope, np.zeros(s.size - envelope.size))
+
+    envelope[envelope > release] = release[envelope > release]
 
     maxs = np.max(s)
-    final_array = [(s/((maxs))*envelope) for s,envelope in zip(s,envelope)]
+    final_array = s / maxs * envelope  # [(s/((maxs))*envelope) for s,envelope in zip(s,envelope)]
 
-    return final_array;
+    return final_array
